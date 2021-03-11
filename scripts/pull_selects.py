73821eb9351d8d8fd0cf7454f17cb5df19e7dcfd
Thomas-McLaughlin <42166193+Thomas-McLaughlin@users.noreply.github.com>
2021-03-11 17:08:53 -0500

    Completed pull_selects.py
    The pull_selects.py can now be run to automatically pull down the categories and subcategories from the grailed site, saving them under 'config/selects.json' for later use.
    
    These selects can now be used to populate the post.html page with the options.
    
    Sizing and other considerations that are specific to each category/subcategory still need to be pulled down and populated.

--- scripts/pull_selects.py
+++ scripts/pull_selects.py
@@ -1,36 +1,60 @@
 """
-This program goes to the grailed site and pulls down the categories that are available to sell
+This program goes to the grailed site and pulls down the categories/subcategories that are available to sell
 """
 from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 import time
-from selenium.webdriver.chrome.options import Options
+import json
 
 
 def get_selects(driver):
+    """
+    Get the categories from the grailed site
+    :param driver:
+    :return a list of the categories available on the grailed site:
+    """
     i = 1
     categories = []
-    while (1):
+    while 1:
         try:
             category = driver.find_element_by_xpath(
                 "//*[@id=\"CategoryPage\"]/div[1]/div/div[3]/div[2]/div/div[1]/div[2]/a[{}]/p".format(i))
-            categories.append(category.text)
+            categories.append(str(category.text))
             i += 1
         except NoSuchElementException:
             break
     return categories
 
 
-def main():
-    option = Options()
-    option.add_argument("--disable-notifications")
-    option.add_argument("start-maximized")
-    option.add_argument("--disable-extensions")
-    option.add_experimental_option("prefs", {"profile.default_content_setting_values.notifications": 1})
-    # option.add_argument('--headless')
-    driver = webdriver.Chrome(options=option)
-    driver.maximize_window()
+def get_subs(driver, cat):
+    """
+    Gets a list of the subcategories for a given category from the grailed site
+    :param driver:
+    :param cat - The name of the category we want the subcategories of:
+    :return a list of the subcategories for the given cat:
+    """
+    cat_to_int = {'tops':'2', 'bottoms':'3', 'outerwear':'4', 'footwear':'5', 'tailoring':'6', 'accessories':'7'}
+    subs = []
+    item = 1
+    while 1:
+        try:
+            category = cat_to_int[cat.lower()]
+            sub = driver.find_element_by_xpath(
+                "//*[@id=\"CategoryPage\"]/div[1]/div/div[3]/div[1]/div[{}]/div/a[{}]".format(category, item)).get_attribute("innerText")
+            subs.append(sub)
+            item += 1
+        except NoSuchElementException:
+            break
+    return subs
 
+
+def main():
+    """
+    Driver for the pull selects script. Generates a dictionary holding the categories and their subcategories
+    :return:
+    """
+    driver = webdriver.Chrome()
+    driver.maximize_window()
     driver.get("https://grailed.com/categories/all")
     time.sleep(3)
 
@@ -39,15 +63,19 @@ def main():
     sub_categories = []
     for cat in categories:
         url = "https://grailed.com/categories/" + str(cat).lower()
-        print(url)
         driver.get(url)
-        time.sleep(10)
-        subs = get_selects(driver)
+        time.sleep(6)
+        subs = get_subs(driver, cat)
         sub_categories.append(subs)
 
     driver.close()
-    [print(cat) for cat in categories]
-    [print(sub) for sub in sub_categories]
+
+    selects = {}
+    for i in range(len(categories)):
+        selects[categories[i]] = sub_categories[i]
+
+    with open("configs/selects.json", "w") as outfile:
+        json.dump(selects, outfile)
 
 
 main()
